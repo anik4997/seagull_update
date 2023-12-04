c1ebf5fc839d8d98deaf2efd9ad2f86eebeb3d8b
anik4997 <44748778+anik4997@users.noreply.github.com>
2023-12-04 15:54:15 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,43 @@ $('.center_slick').slick({
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 3,
+    // dots: true,
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          arrows: true,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 3,
+          variableWidth: true
+        }
+      },
+      {
+      breakpoint: 992,
+      settings: {
+        arrows: true,
+        centerMode: false,
+        centerPadding: '40px',
+        slidesToShow: 3,
+        variableWidth: true
+      }
+    },
+      {
+        // breakpoint: 768,
+        // settings: {
+        //   arrows: false,
+        //   centerMode: true,
+        //   centerPadding: '40px',
+        //   slidesToShow: 3
+        // }
         breakpoint: 768,
         settings: {
-          arrows: false,
-          centerMode: true,
+          arrows: true,
+          centerMode: false,
           centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 1,
+          // variableWidth: true
         }
       },
       {
